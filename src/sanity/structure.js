e7c59e68faa5d6ff595236599e8a06f86d6b60aa
Ho Van Quoc <songtulap001@gmail.com>
2025-11-08 00:18:25 -0800

    Fix bug Sanity

--- src/sanity/structure.js
+++ src/sanity/structure.js
@@ -1,5 +1,16 @@
-// https://www.sanity.io/docs/structure-builder-cheat-sheet
+// structure.js
 export const structure = (S) =>
   S.list()
     .title('Content')
-    .items(S.documentTypeListItems())
+    .items([
+      // Thêm các document types của bạn vào đây
+      ...S.documentTypeListItems(),
+
+      // Hoặc custom structure nếu muốn:
+      // S.listItem()
+      //   .title('Cấu hình SEO')
+      //   .child(S.document().schemaType('settings').documentId('settings')),
+      // S.listItem()
+      //   .title('Trang Giới Thiệu')
+      //   .child(S.documentTypeList('about')),
+    ]);
